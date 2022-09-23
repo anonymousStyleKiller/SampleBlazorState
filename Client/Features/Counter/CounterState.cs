eb5dfc43dfe1c79688bc3d11d1df2286d35c9d8f
anton.kharchenko <gumb1t97@gmail.com>
2022-09-23 11:44:37 +0300

    init

---
+++ Client/Features/Counter/CounterState.cs
@@ -0,0 +1,27 @@
+﻿using BlazorState;
+using MediatR;
+
+namespace Sample.Client.Features.Counter;
+
+public class CounterState : State<CounterState>
+{
+    public int Count { get; set; }
+    public override void Initialize()=>Count = 3;
+    public class IncrementCountAction  : IAction
+    {
+        public int Amount { get; set; }
+    }
+    
+    public class IncrementCountHandler : ActionHandler<IncrementCountAction>
+    {
+        public IncrementCountHandler(IStore aStore) : base(aStore) { }
+
+        CounterState CounterState => Store.GetState<CounterState>();
+
+        public override Task<Unit> Handle(IncrementCountAction aIncrementCountAction, CancellationToken aCancellationToken)
+        {
+            CounterState.Count = CounterState.Count + aIncrementCountAction.Amount;
+            return Unit.Task;
+        }
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Sample.Client;
+using BlazorState;
+using System.Reflection;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+builder.Services.AddBlazorState
+    (
+     (aOptions) =>
+     {
+         aOptions.UseReduxDevToolsBehavior = true;
+         aOptions.Assemblies =
+             new Assembly[]
+             {
+                 typeof(Program).GetTypeInfo().Assembly,
+             };
+     }
+    );
+await builder.Build().RunAsync();
